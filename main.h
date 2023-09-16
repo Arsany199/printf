82a39e31b454e2cc9e8539c2e64353c9a05daf43
Arsany199 <arsanykamal30@gmail.com>
2023-09-16 03:46:10 -0700

    printf

--- main.h
+++ main.h
@@ -1,41 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <unistd.h>
-#include <stdlib.h>
+#include <stdio.h>
 #include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
 
-
-int _strlen(const char *);
-int print(char *);
-char *itoa(long int, int);
-
-
-int _printf(const char *, ...);
-
-
-int handler(const char *, va_list);
-int percent_handler(const char *, va_list, int *);
-
-
-int print_string(va_list);
-int print_char(va_list);
-int print_integer(va_list);
-int print_binary(va_list);
-int print_rot(va_list);
-int print_unsigned(va_list);
-int print_octal(va_list);
-int print_hexadecimal_low(va_list);
-int print_hexadecimal_upp(va_list);
-int print_pointer(va_list);
-int print_rev_string(va_list);
-
-
-typedef struct _format
-{
-	char type;
-	int (*f)(va_list);
-} format;
-
+int _printf(const char *format, ...);
 
 #endif
---
+++ printf.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+int _printf(const char *format, ...)
+{
+	int count = 0;
+	va_list args;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+		}
+		format++;
+	}
+	return (count);
+}
